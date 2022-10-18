e6849c2bf572213d9877d188aaeead97d7afea45
Kasper Lausen <habberlabber@gmail.com>
2022-10-18 19:53:26 +0200

    Stuff

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,8 @@
-import type { LayoutServerLoad } from './$types';
+import type { LayoutServerLoad } from '.svelte-kit/types/src/routes/$types';
 import { getServerSession } from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutServerLoad = async (event) => {
-    return {
-        session: await getServerSession(event)
-    };
+	return {
+		session: await getServerSession(event)
+	};
 };
