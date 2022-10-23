8b5cd17a51508eb7a268f148252f37b30335d7df
Kasper Lausen <habberlabber@gmail.com>
2022-10-23 17:23:05 +0200

    Small fix

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -6,10 +6,11 @@
 
 <header class="flex gap-4 items-center w-full p-4 border-b border-slate-200 bg-white">
 	<h1
-		class="text-2xl grow uppercase font-extrabold md:text-3xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-emerald-500"
+		class="text-2xl uppercase font-extrabold md:text-3xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-emerald-500"
 	>
 		<a href="/">Strømpris</a>
 	</h1>
+	<span class="grow" />
 
 	{#if !$page.data.session}
 		<Link href="/login" color="PRIMARY">Login</Link>
