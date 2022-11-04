f27b5687a1c9344ac91d708d160f4ce5597d4226
Kasper <habberlabber@gmail.com>
2022-11-04 19:41:53 +0100

    Fix area switching

--- src/lib/components/charts/DailySpot.svelte
+++ src/lib/components/charts/DailySpot.svelte
@@ -8,7 +8,8 @@
 		BarElement,
 		CategoryScale,
 		LinearScale,
-		type ChartData
+		type ChartData,
+		type ChartDataset
 	} from 'chart.js';
 	import ChartDataLabels from 'chartjs-plugin-datalabels';
 	import type { PriceAreas } from '$lib/energidataservice/types';
@@ -20,76 +21,80 @@
 	export let spotData: { priceArea: PriceAreas; priceDKK: number; hourUTC: DateTime }[] = [];
 	export let feeData: { [fee in FeeKeys]: number };
 
-	const sortedData = spotData
-		.sort((a, b) =>
-			a.hourUTC.setZone('Europe/Copenhagen').hour > b.hourUTC.setZone('Europe/Copenhagen').hour
-				? 1
-				: -1
-		)
-		.reverse();
+	let data: ChartData<'bar'>;
 
-	const isToday: boolean =
-		sortedData[0].hourUTC.setZone('Europe/Copenhagen').day === DateTime.now().day;
+	$: {
+		const sortedData = spotData
+			.sort((a, b) =>
+				a.hourUTC.setZone('Europe/Copenhagen').hour > b.hourUTC.setZone('Europe/Copenhagen').hour
+					? 1
+					: -1
+			)
+			.reverse();
 
-	const getDataColors = (r: number, g: number, b: number) =>
-		isToday
-			? sortedData.map(({ hourUTC }) =>
-					hourUTC.setZone('Europe/Copenhagen').hour < DateTime.now().hour
-						? `rgb(${r}, ${g}, ${b}, 0.5)`
-						: `rgba(${r}, ${g}, ${b}, 1)`
-			  )
-			: `rgba(${r}, ${g}, ${b}, 1)`;
+		const isToday: boolean =
+			sortedData[0].hourUTC.setZone('Europe/Copenhagen').day === DateTime.now().day;
+
+		const getDataColors = (r: number, g: number, b: number) =>
+			isToday
+				? sortedData.map(({ hourUTC }) =>
+						hourUTC.setZone('Europe/Copenhagen').hour < DateTime.now().hour
+							? `rgb(${r}, ${g}, ${b}, 0.5)`
+							: `rgba(${r}, ${g}, ${b}, 1)`
+				  )
+				: `rgba(${r}, ${g}, ${b}, 1)`;
+
+		const elafgifter = sortedData.map(() => feeData.elafgift / 100);
+		const transmissiionstariffer = sortedData.map(() => feeData.transmissionstarif / 100);
+		const systemtariffer = sortedData.map(() => feeData.systemtarif / 100);
+		const spot = sortedData.map(({ priceDKK }) => Number((priceDKK / 1000).toFixed(2)));
+		const moms = sortedData.map(
+			(value, index) =>
+				(elafgifter[index] + transmissiionstariffer[index] + systemtariffer[index] + spot[index]) *
+				0.25
+		);
+
+		const datasets = [
+			{
+				label: 'ElAfgift',
+				data: elafgifter,
+				backgroundColor: getDataColors(52, 211, 153)
+			},
+			{
+				label: 'Transmissionstarif',
+				data: transmissiionstariffer,
+				backgroundColor: getDataColors(34, 211, 238)
+			},
+			{
+				label: 'Systemtarif',
+				data: systemtariffer,
+				backgroundColor: getDataColors(129, 140, 248)
+			},
+			{
+				label: 'Moms',
+				data: moms,
+				backgroundColor: getDataColors(251, 191, 36)
+			},
+			{
+				label: 'Spot',
+				data: spot,
+				backgroundColor: getDataColors(148, 163, 184)
+			}
+		];
+
+		data = {
+			labels: sortedData.map(({ hourUTC }) => {
+				const hour = hourUTC.setZone('Europe/Copenhagen').hour;
+				return `kl: ${`0${hour}`.slice(-2)} - ${`0${hour + 1}`.slice(-2)}`;
+			}),
+			datasets: datasets
+		};
+	}
+
+	const getTotalByIndex = (index: number, allData: ChartDataset<'bar', number[]>[]) =>
+		allData.reduce((previous, current) => current.data[index] + previous, 0);
 
 	Chart.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, ChartDataLabels);
-
-	const elafgifter = sortedData.map(() => feeData.elafgift / 100);
-	const transmissiionstariffer = sortedData.map(() => feeData.transmissionstarif / 100);
-	const systemtariffer = sortedData.map(() => feeData.systemtarif / 100);
-	const spot = sortedData.map(({ priceDKK }) => Number((priceDKK / 1000).toFixed(2)));
-	const moms = sortedData.map(
-		(value, index) =>
-			(elafgifter[index] + transmissiionstariffer[index] + systemtariffer[index] + spot[index]) *
-			0.25
-	);
-
-	const datasets = [
-		{
-			label: 'ElAfgift',
-			data: elafgifter,
-			backgroundColor: getDataColors(52, 211, 153)
-		},
-		{
-			label: 'Transmissionstarif',
-			data: transmissiionstariffer,
-			backgroundColor: getDataColors(34, 211, 238)
-		},
-		{
-			label: 'Systemtarif',
-			data: systemtariffer,
-			backgroundColor: getDataColors(129, 140, 248)
-		},
-		{
-			label: 'Moms',
-			data: moms,
-			backgroundColor: getDataColors(251, 191, 36)
-		},
-		{
-			label: 'Spot',
-			data: spot,
-			backgroundColor: getDataColors(148, 163, 184)
-		}
-	];
-
-	const getTotalByIndex = (index: number) =>
-		datasets.reduce((previous, current) => current.data[index] + previous, 0);
-
-	const data: ChartData<'bar'> = {
-		labels: sortedData.map(({ hourUTC }) => {
-			const hour = hourUTC.setZone('Europe/Copenhagen').hour;
-			return `kl: ${`0${hour}`.slice(-2)} - ${`0${hour + 1}`.slice(-2)}`;
-		}),
-		datasets: datasets
-	};
 </script>
 
 <div class="overflow-hidden">
@@ -117,7 +122,7 @@
 					align: 'end',
 					formatter: (value, context) => {
 						if (context.dataset.label?.includes('Spot')) {
-							return `${getTotalByIndex(context.dataIndex).toFixed(2)} kr/kwh`;
+							return `${getTotalByIndex(context.dataIndex, data.datasets).toFixed(2)} kr/kwh`;
 						} else {
 							return '';
 						}
