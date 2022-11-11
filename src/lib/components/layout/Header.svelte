32b8e52c7e67020e3c7add943555184084bbc0a2
Kasper <habberlabber@gmail.com>
2022-11-11 23:32:58 +0100

    better mobile

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -7,7 +7,9 @@
 	import { Cog, Squares2x2, ArrowRightOnRectangle, Bolt } from '@steeze-ui/heroicons';
 </script>
 
-<header class="flex gap-4 items-center w-full p-4  bg-transparent">
+<header
+	class="flex flex-col md:flex-row md:justify-between gap-4 items-center w-full p-4 bg-transparent "
+>
 	<h1
 		class="text-2xl uppercase font-extrabold md:text-3xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-emerald-500"
 	>
@@ -19,22 +21,23 @@
 			/>Strømpris</a
 		>
 	</h1>
-	<span class="grow" />
 
-	{#if !$page.data.session}
-		<Link href="/login" color="PRIMARY" title="Log ind">Log ind</Link>
-	{:else}
-		<Link color="SECONDARY" href="/profile" title="Indstillinger">
-			<Icon src={Cog} theme="solid" class={`h-4 w-4`} />
-		</Link>
-		<Link color="SECONDARY" href="/dashboard" title="Personlig data">
-			<Icon src={Squares2x2} theme="solid" class={`h-4 w-4`} />
-		</Link>
-		<form method="POST" action="/signout">
-			<Button color="SECONDARY" title="Log ud" class="flex group">
-				<Icon src={ArrowRightOnRectangle} theme="solid" class={`h-4 w-4`} />
-			</Button>
-		</form>
-	{/if}
-	<ThemeToggle />
+	<span class="flex gap-4">
+		{#if !$page.data.session}
+			<Link href="/login" color="PRIMARY" title="Log ind">Log ind</Link>
+		{:else}
+			<Link color="SECONDARY" href="/profile" title="Indstillinger">
+				<Icon src={Cog} theme="solid" class={`h-4 w-4`} />
+			</Link>
+			<Link color="SECONDARY" href="/dashboard" title="Personlig data">
+				<Icon src={Squares2x2} theme="solid" class={`h-4 w-4`} />
+			</Link>
+			<form method="POST" action="/signout">
+				<Button color="SECONDARY" title="Log ud" class="flex group">
+					<Icon src={ArrowRightOnRectangle} theme="solid" class={`h-4 w-4`} />
+				</Button>
+			</form>
+		{/if}
+		<ThemeToggle />
+	</span>
 </header>
