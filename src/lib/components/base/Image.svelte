773b15704bbf684fd6d9e5680f267294290ca59e
Kasper <habberlabber@gmail.com>
2022-11-17 16:16:00 +0100

    Add close link to Image

--- src/lib/components/base/Image.svelte
+++ src/lib/components/base/Image.svelte
@@ -14,5 +14,8 @@
         ${active ? `z-50 fixed inset-0 p-4 md:p-12 xl:p-32 bg-black bg-opacity-80` : `max-w-3xl`}
         `}
 >
-	<img {alt} {src} class={`max-w-full mx-auto `} />
+	<p class={`text-lg p-4 text-white text-end pointer-events-none  ${active ? `block` : `hidden`}`}>
+		Skjul billede
+	</p>
+	<img {alt} {src} class={`max-w-full mx-auto ${active ? `rounded-xl` : `rounded-lg`}`} />
 </div>
