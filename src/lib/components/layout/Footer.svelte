1bbf6dc62c15e26a4f0c0f1f6b3e1123b8394350
Kasper <habberlabber@gmail.com>
2022-11-17 21:26:05 +0100

    Small fixes

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <footer
-	class="bg-white dark:bg-neutral-900 dark:border-neutral-700 border-slate-200 p-4 gap-4 flex justify-between border-t flex-wrap"
+	class="bg-white dark:bg-neutral-900 dark:border-neutral-700 border-slate-200 p-4 px-12 gap-4 flex justify-between border-t flex-wrap"
 >
 	<P class="flex gap-2 flex-wrap">
 		Kilder:
@@ -15,15 +15,25 @@
 			Spot Priser fra Energidataservice.dk
 		</Link> |
 		<Link href="https://skat.dk/data.aspx?oid=2234584">ElAfgift</Link>
+		|
+		<Link href="https://eloverblik.dk/welcome">Eloverblik</Link>
 	</P>
-	<P>
-		Created by <Link
+	<P class="flex gap-2 flex-wrap">
+		<Link
 			class="text-sky-600"
 			target="_blank"
 			rel="noreferrer"
 			href="https://www.linkedin.com/in/habber/"
 		>
 			Kasper Laursen
+		</Link> <span>|</span>
+		<Link
+			class="text-sky-600"
+			target="_blank"
+			rel="noreferrer"
+			href="https://github.com/kasperlaursen/stroem-pris"
+		>
+			GitHub
 		</Link>
 	</P>
 </footer>
--- src/routes/accept/+page.svelte
+++ src/routes/accept/+page.svelte
@@ -1,21 +1,12 @@
 <script lang="ts">
-	import type { ActionData } from '.svelte-kit/types/src/routes/$types';
 	import { Card, Heading, Input, Button, Label, P, InputAddon, ButtonGroup } from 'flowbite-svelte';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { Key } from '@steeze-ui/heroicons';
-
-	export let form: ActionData;
+	import type { ActionData } from '.svelte-kit/types/src/routes/accept/$types';
 </script>
 
 <main class="grid justify-items-center gap-4">
 	<Heading customSize="text-2xl font-bold" class="text-center">Velkommen til strømpris! 👋</Heading>
-
-	{#if form && form?.error}
-		<div>
-			<p>Password set failed...</p>
-			<p>{form.error}</p>
-		</div>
-	{/if}
 
 	<Card class="w-96 grid gap-4">
 		<P>Udfyld venligst dit ønskede kodeord nedenfor for at færdiggøre oprettelsen af din bruger.</P>
