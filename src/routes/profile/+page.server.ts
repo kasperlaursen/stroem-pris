291de2b7c9bb873814c9dd60f482fdaef3579f0f
Kasper <habberlabber@gmail.com>
2022-11-17 19:48:02 +0100

    Add price area setting to settings page

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -3,6 +3,29 @@ import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 import { error, invalid, redirect } from '@sveltejs/kit';
 
 export const actions: Actions = {
+	updateSettings: async (event) => {
+		const { request } = event;
+		const { session, supabaseClient } = await getSupabase(event);
+		if (!session) {
+			// the user is not signed in
+			throw error(403, { message: 'Unauthorized' });
+		}
+		// we are save, let the user create the post
+		const formData = await request.formData();
+		const area = formData.get('area') === 'DK2' ? 'DK2' : 'DK1';
+
+		const { error: setSettingsError } = await supabaseClient
+			.from('user_settings')
+			.upsert({ user_id: session.user.id, price_area: area });
+
+		if (setSettingsError) {
+			console.log(setSettingsError);
+			return invalid(500, {
+				supabaseErrorMessage: setSettingsError.message
+			});
+		}
+		throw redirect(303, '/profile');
+	},
 	setToken: async (event) => {
 		const { request } = event;
 		const { session, supabaseClient } = await getSupabase(event);
--- src/routes/profile/+page.ts
+++ src/routes/profile/+page.ts
@@ -1,6 +1,7 @@
 import type { PageLoad } from './$types';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 import { redirect } from '@sveltejs/kit';
+import type { PriceAreas } from '$lib/energidataservice/types';
 
 export const load: PageLoad = async (event) => {
 	const { session, supabaseClient } = await getSupabase(event);
@@ -11,7 +12,16 @@ export const load: PageLoad = async (event) => {
 		.from('datahub_tokens')
 		.select('refresh_token, usage_meter_id');
 
+	const { data: settingData, error } = await supabaseClient
+		.from('user_settings')
+		.select('price_area');
+
+	console.log('settingData', settingData);
+
+	const priceArea: PriceAreas = settingData?.[0]?.price_area ?? 'DK1';
+
 	return {
+		priceArea,
 		user: session.user,
 		meterId: tokenData?.[0]?.usage_meter_id ?? '',
 		hasToken: Boolean(tokenData && tokenData?.[0]?.refresh_token)
