83ce80d1dcf36d9f8bc13c02c2137a1be9a1956d
Kasper <habberlabber@gmail.com>
2023-01-05 17:00:18 +0100

    More visible active row

--- src/lib/components/Charts/SpotChart/SpotChartRow.svelte
+++ src/lib/components/Charts/SpotChart/SpotChartRow.svelte
@@ -5,14 +5,18 @@
 	export let state: RowState = 'none';
 	const stateClasses: { [key in RowState]: string } = {
 		none: '',
-		active: 'bg-neutral-300 bg-opacity-20 py-1 animate-pulse is-active',
+		active: 'bg-neutral-300 bg-opacity-30 py-1 animate-pulse is-active',
 		inactive: 'opacity-50'
 	};
 </script>
 
 <div
 	{...$$restProps}
-	class={`${customClasses} group grid grid-cols-[max-content,max-content,auto] gap-4 px-4 items-center rounded ${stateClasses[state]}`}
+	class={`${
+		customClasses ?? ''
+	} group grid grid-cols-[max-content,max-content,auto] gap-4 px-4 items-center rounded ${
+		stateClasses[state]
+	}`}
 >
 	<slot />
 </div>
