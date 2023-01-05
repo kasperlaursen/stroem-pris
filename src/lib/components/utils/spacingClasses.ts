621c25a0c830ba82f7ad114eb3f95164bea7d492
Kasper <habberlabber@gmail.com>
2023-01-05 17:01:05 +0100

    spacing classes

---
+++ src/lib/components/utils/spacingClasses.ts
@@ -0,0 +1,3 @@
+export const responsiveGapClasses = 'gap-1 sm:gap-2 md:gap-4';
+export const responsivePaddingClasses = 'p-1 sm:p-2 md:p-4';
+export const responsiveSpacingClasses = `${responsiveGapClasses} ${responsivePaddingClasses}`;
