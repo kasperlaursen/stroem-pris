e4b6bc717f08ddf4a8c74f41ae30eb6f60549e1e
Kasper <habberlabber@gmail.com>
2023-01-05 17:00:44 +0100

    Input component

---
+++ src/lib/components/Input/Input.story.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Input from './Input.svelte';
+	import type { Hst } from '@histoire/plugin-svelte';
+	import type { InputTypes } from './types';
+	export let Hst: Hst;
+
+	let type: InputTypes = 'text';
+	const typeOptions: { label: InputTypes; value: InputTypes }[] = [
+		{ label: 'text', value: 'text' },
+		{ label: 'number', value: 'number' },
+		{ label: 'date', value: 'date' }
+	];
+</script>
+
+<Hst.Story>
+	<Input {type} />
+	<svelte:fragment slot="controls">
+		<Hst.Select options={typeOptions} bind:value={type} title="type" />
+	</svelte:fragment>
+</Hst.Story>
---
+++ src/lib/components/Input/Input.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { InputTypes } from './types';
+
+	const customClasses = $$restProps.class;
+	export let type: InputTypes;
+</script>
+
+<input
+	{...$$restProps}
+	{type}
+	class={`
+        ${customClasses ?? ''}
+        text-sm
+        py-1
+        px-2
+        m-1
+        rounded-md
+        border
+        bg-white
+        dark:bg-neutral-800
+        border-gray-300
+        dark:border-gray-600
+        focus:ring-2
+        focus:border-primary-500 
+        focus:ring-primary-500
+    `}
+/>
