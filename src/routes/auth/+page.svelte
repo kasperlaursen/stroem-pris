6b8c34be3cf41c3feef4a18b0d0ab07781011925
Kasper <habberlabber@gmail.com>
2023-01-11 20:15:48 +0100

    Login form

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import Button from '$lib/components/Button/Button.svelte';
+	import Card from '$lib/components/Card/Card.svelte';
+	import Fieldset from '$lib/components/Fieldset/Fieldset.svelte';
+	import Input from '$lib/components/Input/Input.svelte';
+	import Label from '$lib/components/Label/Label.svelte';
 	import type { ActionData } from './$types';
 	export let form: ActionData;
 </script>
@@ -7,10 +12,16 @@
 	<p>{form.error.message}</p>
 {/if}
 
-<form method="POST" action="?/signin" class="grid gap-4">
-	<label for="email">E-mail:</label>
-	<input id="email" name="email" type="email" placeholder="E-mail" />
-	<label for="password">Kodeord:</label>
-	<input id="password" name="password" type="password" placeholder="Kodeord" />
-	<button type="submit">Log in</button>
-</form>
+<Card class="w-full max-w-md mx-auto self-center mt-20">
+	<form method="POST" action="?/signin" class="grid gap-4">
+		<Fieldset>
+			<Label for="email">E-mail:</Label>
+			<Input id="email" name="email" type="email" placeholder="john.doe@gmail.com" />
+		</Fieldset>
+		<Fieldset>
+			<Label for="password">Kodeord:</Label>
+			<Input id="password" name="password" type="password" placeholder="******" />
+		</Fieldset>
+		<Button type="submit">Log in</Button>
+	</form>
+</Card>
