4eca905129b8dd3d45fec52e6a103d1bd59979e7
Kasper <habberlabber@gmail.com>
2023-02-17 21:30:55 +0100

    Test filterDataToInterval

---
+++ src/lib/data/spot/filterDataToInterval.test.ts
@@ -0,0 +1,36 @@
+import { DateTime } from 'luxon';
+import { describe, it, expect } from 'vitest';
+import { filterDataToInterval } from './filterDataToInterval';
+
+describe('filterDataToInterval', () => {
+	it('should return points within range', () => {
+		const from = DateTime.fromISO('2022-01-01T01:00:00.000Z');
+		const to = DateTime.fromISO('2022-01-01T03:00:00.000Z');
+		const result = filterDataToInterval({
+			from,
+			to,
+			data: [
+				{
+					hourUTC: DateTime.fromISO('2022-01-01T00:00:00.000Z').toJSDate()
+				},
+				{
+					hourUTC: DateTime.fromISO('2022-01-01T01:00:00.000Z').toJSDate()
+				},
+				{
+					hourUTC: DateTime.fromISO('2022-01-01T02:00:00.000Z').toJSDate()
+				},
+				{
+					hourUTC: DateTime.fromISO('2022-01-01T03:00:00.000Z').toJSDate()
+				}
+			]
+		});
+		expect(result).toEqual([
+			{
+				hourUTC: DateTime.fromISO('2022-01-01T01:00:00.000Z').toJSDate()
+			},
+			{
+				hourUTC: DateTime.fromISO('2022-01-01T02:00:00.000Z').toJSDate()
+			}
+		]);
+	});
+});
