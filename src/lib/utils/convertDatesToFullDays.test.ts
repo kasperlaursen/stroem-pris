6fe859850892a2598f244d321ac622ed902aa019
Kasper <habberlabber@gmail.com>
2023-02-23 21:21:27 +0100

    Add more tests

--- src/lib/utils/convertDatesToFullDays.test.ts
+++ src/lib/utils/convertDatesToFullDays.test.ts
@@ -11,16 +11,10 @@ describe('convertDatesToFullDays', () => {
 			to
 		});
 
-		const expected: ReturnType<typeof convertDatesToFullDays> = {
-			fullDayFrom: DateTime.fromISO('2022-01-01T00:00:00.000Z', { zone: 'UTC' }),
-			fullDayTo: DateTime.fromISO('2022-01-02T00:00:00.000Z', { zone: 'UTC' }),
-			fullHourDiff: 24
-		};
-
-		expect(result).toEqual(expected);
+		expect(result.fullHourDiff).toEqual(24);
 	});
 
-	it('returns two days for hours overlapping two dates', () => {
+	it('returns two days for hours overlapping two dates (DK winter time)', () => {
 		const from = DateTime.fromISO('2022-01-02T23:00:00.000Z', { zone: 'UTC' });
 		const to = DateTime.fromISO('2022-01-03T01:00:00.000Z', { zone: 'UTC' });
 		const result = convertDatesToFullDays({
@@ -29,15 +23,17 @@ describe('convertDatesToFullDays', () => {
 		});
 
 		const expected: ReturnType<typeof convertDatesToFullDays> = {
-			fullDayFrom: DateTime.fromISO('2022-01-02T00:00:00.000Z', { zone: 'UTC' }),
-			fullDayTo: DateTime.fromISO('2022-01-04T00:00:00.000Z', { zone: 'UTC' }),
+			fullDayFrom: DateTime.fromISO('2022-01-02T00:00:00.000+01:00', { zone: 'Europe/Copenhagen' }),
+			fullDayFromUTC: DateTime.fromISO('2022-01-01T23:00:00.000Z', { zone: 'UTC' }),
+			fullDayTo: DateTime.fromISO('2022-01-04T00:00:00.000+01:00', { zone: 'Europe/Copenhagen' }),
+			fullDayToUTC: DateTime.fromISO('2022-01-03T23:00:00.000Z', { zone: 'UTC' }),
 			fullHourDiff: 48
 		};
 
 		expect(result).toEqual(expected);
 	});
 
-	it('returns correct days over month change', () => {
+	it('returns correct days over month change (DK winter time)', () => {
 		const from = DateTime.fromISO('2022-01-31T23:00:00.000Z', { zone: 'UTC' });
 		const to = DateTime.fromISO('2022-02-01T01:00:00.000Z', { zone: 'UTC' });
 		const result = convertDatesToFullDays({
@@ -46,8 +42,48 @@ describe('convertDatesToFullDays', () => {
 		});
 
 		const expected: ReturnType<typeof convertDatesToFullDays> = {
-			fullDayFrom: DateTime.fromISO('2022-01-31T00:00:00.000Z', { zone: 'UTC' }),
-			fullDayTo: DateTime.fromISO('2022-02-02T00:00:00.000Z', { zone: 'UTC' }),
+			fullDayFrom: DateTime.fromISO('2022-01-31T00:00:00.000+01:00', { zone: 'Europe/Copenhagen' }),
+			fullDayFromUTC: DateTime.fromISO('2022-01-30T23:00:00.000Z', { zone: 'UTC' }),
+			fullDayTo: DateTime.fromISO('2022-02-02T00:00:00.000+01:00', { zone: 'Europe/Copenhagen' }),
+			fullDayToUTC: DateTime.fromISO('2022-02-01T23:00:00.000Z', { zone: 'UTC' }),
+			fullHourDiff: 48
+		};
+
+		expect(result).toEqual(expected);
+	});
+
+	it('returns two days for hours overlapping two dates (DK summer time)', () => {
+		const from = DateTime.fromISO('2022-05-02T23:00:00.000Z', { zone: 'UTC' });
+		const to = DateTime.fromISO('2022-05-03T01:00:00.000Z', { zone: 'UTC' });
+		const result = convertDatesToFullDays({
+			from,
+			to
+		});
+
+		const expected: ReturnType<typeof convertDatesToFullDays> = {
+			fullDayFrom: DateTime.fromISO('2022-05-02T00:00:00.000+02:00', { zone: 'Europe/Copenhagen' }),
+			fullDayFromUTC: DateTime.fromISO('2022-05-01T22:00:00.000Z', { zone: 'UTC' }),
+			fullDayTo: DateTime.fromISO('2022-05-04T00:00:00.000+02:00', { zone: 'Europe/Copenhagen' }),
+			fullDayToUTC: DateTime.fromISO('2022-05-03T22:00:00.000Z', { zone: 'UTC' }),
+			fullHourDiff: 48
+		};
+
+		expect(result).toEqual(expected);
+	});
+
+	it('returns correct days over month change (DK summer time)', () => {
+		const from = DateTime.fromISO('2022-05-31T23:00:00.000Z', { zone: 'UTC' });
+		const to = DateTime.fromISO('2022-06-01T01:00:00.000Z', { zone: 'UTC' });
+		const result = convertDatesToFullDays({
+			from,
+			to
+		});
+
+		const expected: ReturnType<typeof convertDatesToFullDays> = {
+			fullDayFrom: DateTime.fromISO('2022-05-31T00:00:00.000+02:00', { zone: 'Europe/Copenhagen' }),
+			fullDayFromUTC: DateTime.fromISO('2022-05-30T22:00:00.000Z', { zone: 'UTC' }),
+			fullDayTo: DateTime.fromISO('2022-06-02T00:00:00.000+02:00', { zone: 'Europe/Copenhagen' }),
+			fullDayToUTC: DateTime.fromISO('2022-06-01T22:00:00.000Z', { zone: 'UTC' }),
 			fullHourDiff: 48
 		};
 
@@ -63,9 +99,49 @@ describe('convertDatesToFullDays', () => {
 		});
 
 		const expected: ReturnType<typeof convertDatesToFullDays> = {
-			fullDayFrom: DateTime.fromISO('2021-12-31T00:00:00.000Z', { zone: 'UTC' }),
-			fullDayTo: DateTime.fromISO('2022-01-02T00:00:00.000Z', { zone: 'UTC' }),
+			fullDayFrom: DateTime.fromISO('2021-12-31T00:00:00.000+01:00', { zone: 'Europe/Copenhagen' }),
+			fullDayFromUTC: DateTime.fromISO('2021-12-30T23:00:00.000Z', { zone: 'UTC' }),
+			fullDayTo: DateTime.fromISO('2022-01-02T00:00:00.000+01:00', { zone: 'Europe/Copenhagen' }),
+			fullDayToUTC: DateTime.fromISO('2022-01-01T23:00:00.000Z', { zone: 'UTC' }),
 			fullHourDiff: 48
+		};
+
+		expect(result).toEqual(expected);
+	});
+
+	it('returns correct days over winter time -> summer time change', () => {
+		const from = DateTime.fromISO('2023-03-25T23:00:00.000Z', { zone: 'UTC' });
+		const to = DateTime.fromISO('2023-03-26T01:00:00.000Z', { zone: 'UTC' });
+		const result = convertDatesToFullDays({
+			from,
+			to
+		});
+
+		const expected: ReturnType<typeof convertDatesToFullDays> = {
+			fullDayFrom: DateTime.fromISO('2023-03-25T00:00:00.000+01:00', { zone: 'Europe/Copenhagen' }),
+			fullDayFromUTC: DateTime.fromISO('2023-03-24T23:00:00.000Z', { zone: 'UTC' }),
+			fullDayTo: DateTime.fromISO('2023-03-27T00:00:00.000+02:00', { zone: 'Europe/Copenhagen' }),
+			fullDayToUTC: DateTime.fromISO('2023-03-26T22:00:00.000Z', { zone: 'UTC' }),
+			fullHourDiff: 47
+		};
+
+		expect(result).toEqual(expected);
+	});
+
+	it('returns correct days over summer time -> winter time change', () => {
+		const from = DateTime.fromISO('2023-10-28T23:00:00.000Z', { zone: 'UTC' });
+		const to = DateTime.fromISO('2023-10-29T01:00:00.000Z', { zone: 'UTC' });
+		const result = convertDatesToFullDays({
+			from,
+			to
+		});
+
+		const expected: ReturnType<typeof convertDatesToFullDays> = {
+			fullDayFrom: DateTime.fromISO('2023-10-28T00:00:00.000+02:00', { zone: 'Europe/Copenhagen' }),
+			fullDayFromUTC: DateTime.fromISO('2023-10-27T22:00:00.000Z', { zone: 'UTC' }),
+			fullDayTo: DateTime.fromISO('2023-10-30T00:00:00.000+01:00', { zone: 'Europe/Copenhagen' }),
+			fullDayToUTC: DateTime.fromISO('2023-10-29T23:00:00.000Z', { zone: 'UTC' }),
+			fullHourDiff: 49
 		};
 
 		expect(result).toEqual(expected);
