72d6e633dcc8f5edbc212521062b30d595178973
Kasper <habberlabber@gmail.com>
2023-02-23 21:33:26 +0100

    Seperate spotResponseToSpotData and add tests

--- src/lib/data/spot/getForDateRange.ts
+++ src/lib/data/spot/getForDateRange.ts
@@ -10,6 +10,7 @@ import { filterSpotData } from './filterSpotData';
 import { saveSpotDataToDatabasse } from './saveSpotDataToDatabasse';
 import { convertDatesToFullDays } from '$lib/utils/convertDatesToFullDays';
 import { filterDataToInterval } from './filterDataToInterval';
+import { spotResponseToSpotData } from './spotResponseToSpotData';
 
 export interface Params extends SpotBaseParams, SupabaseBaseParams {}
 
@@ -88,22 +89,4 @@ export const getForDateRange = async (params: Params): Promise<InternalResponse<
 	const requestedDatapoints = filterDataToInterval({ from, to, data: allDatapoints });
 
 	return { success: true, data: requestedDatapoints };
-};
-
-/** Converts a SpotResponse from the API to a SpotData array */
-const spotResponseToSpotData = (input: SpotResponse): SpotData[] => {
-	if (!input.records || !input.total) {
-		return [];
-	}
-	const spotData = input.records.map(spotDataRawToSpotData);
-	return spotData;
-};
-
-/** Converts a raw spot data entry from the API to spot data */
-const spotDataRawToSpotData = ({ HourUTC, PriceArea, SpotPriceDKK }: SpotDataRaw): SpotData => {
-	return {
-		hourUTC: DateTime.fromISO(HourUTC, { zone: 'UTC' }).toJSDate(),
-		priceArea: PriceArea,
-		priceDKK: SpotPriceDKK
-	};
 };
---
+++ src/lib/data/spot/spotResponseToSpotData.ts
@@ -0,0 +1,21 @@
+import { DateTime } from 'luxon';
+import type { SpotDataRaw, SpotResponse } from './energidataservice/types';
+import type { SpotData } from './types';
+
+/** Converts a SpotResponse from the API to a SpotData array */
+export const spotResponseToSpotData = (input: SpotResponse): SpotData[] => {
+	if (!input.records || !input.total) {
+		return [];
+	}
+	const spotData = input.records.map(spotDataRawToSpotData);
+	return spotData;
+};
+
+/** Converts a raw spot data entry from the API to spot data */
+const spotDataRawToSpotData = ({ HourUTC, PriceArea, SpotPriceDKK }: SpotDataRaw): SpotData => {
+	return {
+		hourUTC: DateTime.fromISO(HourUTC, { zone: 'UTC' }).toJSDate(),
+		priceArea: PriceArea,
+		priceDKK: SpotPriceDKK
+	};
+};
