b47cb9b05e61d606a22c44451849b80043393ad5
Kasper <habberlabber@gmail.com>
2023-02-24 19:46:48 +0100

    update e2e test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
+test('home page has Strømpris in title', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+	expect(await page.textContent('h1')).toContain('Strømpris');
 });
