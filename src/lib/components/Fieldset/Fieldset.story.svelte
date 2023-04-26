1cec8582153e3ddc99b9ea738b3a5a5b9a250eef
Kasper Laursen <habberlabber@gmail.com>
2023-04-26 17:14:21 +0200

    Add Fieldset direction prop

--- src/lib/components/Fieldset/Fieldset.story.svelte
+++ src/lib/components/Fieldset/Fieldset.story.svelte
@@ -3,12 +3,25 @@
 	import Label from '../Label/Label.svelte';
 	import Input from '../Input/Input.svelte';
 	import type { Hst as HstType } from '@histoire/plugin-svelte';
+	import type { Direction } from './types';
 	export let Hst: HstType;
+
+	let direction: Direction = 'stacked';
 </script>
 
 <Hst.Story>
-	<Fieldset>
+	<Fieldset {direction}>
 		<Label>Label text</Label>
 		<Input type="text" placeholder="Text field" />
 	</Fieldset>
+	<svelte:fragment slot="controls">
+		<Hst.Select
+			options={[
+				{ label: 'stacked', value: 'stacked' },
+				{ label: 'inline', value: 'inline' }
+			]}
+			bind:value={direction}
+			title="Spacing"
+		/>
+	</svelte:fragment>
 </Hst.Story>
--- src/lib/components/Fieldset/Fieldset.svelte
+++ src/lib/components/Fieldset/Fieldset.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
 	import { classFromProps } from '$lib/utils/classFromProps';
-
+	import type { Direction } from './types';
+	export let direction: Direction = 'stacked';
 	const customClasses: string = classFromProps($$restProps);
+	const directionMap = {
+		stacked: 'flex-col',
+		inline: 'flex-row items-center gap-2'
+	};
 </script>
 
 <div
 	{...$$restProps}
 	class={`
         ${customClasses}
-        grid
+		${directionMap[direction]}
+        flex
     `}
 >
 	<slot />
